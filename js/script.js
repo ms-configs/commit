4b2f5876ee9fb8e44f667b7a964458b62ed68021
Ivan <example@gmail.com>
2021-03-22 18:15:29 +0300

    firstcommit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+'use strict';
+// a = 15;
+let number = 5;
+const leftBorderWidth = 1;
+number = 10;
+const obj = {
+    a: 50,
+    b: 40,
+};
+
+obj.a = 10;
+console.log(obj);
+
+var name = 'Ivan';
+name = 'Max';
+let goin = Infinity;
+
+// alert('Hello');
+// let bi = confirm('Are you here?');
+// let answer = prompt('Are you 18?', '');
+// console.log(answer);
+//вся информация от пользователя приходит в виде строки
+
+/* const answers = [];
+answers[0] = prompt("what's your name", '');
+answers[1] = prompt('Your surname', '');
+
+const category = 'toys';
+console.log(`https://someurl.com?${category}/5`);
+const user = 'Ivan';
+alert(`Hello ${user}`); */
+
+console.log('arr' + ' - object');
